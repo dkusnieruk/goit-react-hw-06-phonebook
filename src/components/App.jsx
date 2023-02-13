98b76b45f0a48370562ad70ed15bd062b71ebbb2
dkusnieruk <dariusz.kusnieruk@wp.pl>
2023-02-13 19:12:26 +0100

    update task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,36 @@
-import { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
+// import { useState, useEffect } from 'react';
+// import { nanoid } from 'nanoid';
 import { Contacts } from 'features/Contacts/Contacts';
 import ContactList from './ContactList/ContactList2';
 import { Filters } from 'features/Filter/Filter';
 
 function App() {
-  const getStorage = localStorage.getItem('contact');
-  let [contacts, setContacts] = useState(JSON.parse(getStorage));
+  // const getStorage = localStorage.getItem('contact');
+  // let [contacts, setContacts] = useState(JSON.parse(getStorage));
  
-  useEffect(() => {
-    localStorage.setItem(`contact`, JSON.stringify(contacts));
-  }, [contacts]);
+  // useEffect(() => {
+  //   localStorage.setItem(`contact`, JSON.stringify(contacts));
+  // }, [contacts]);
 
-  const onSubmit = (name, number) => {
-    const newContact = {
-      id: nanoid(),
-      name: name,
-      number: number,
-    };
-    const checkArray = contacts.filter(contact => {
-      const filterArray = contact.name.toLowerCase();
-      const filterName = newContact.name.toLowerCase();
+  // const onSubmit = (name, number) => {
+  //   const newContact = {
+  //     id: nanoid(),
+  //     name: name,
+  //     number: number,
+  //   };
+  //   const checkArray = contacts.filter(contact => {
+  //     const filterArray = contact.name.toLowerCase();
+  //     const filterName = newContact.name.toLowerCase();
 
-      if (filterArray.includes(filterName)) {
-        return true;
-      } else return false;
-    });
+  //     if (filterArray.includes(filterName)) {
+  //       return true;
+  //     } else return false;
+  //   });
 
-    if (checkArray.length > 0) {
-      alert(`Masz już kontakt o imieniu : ${newContact.name}`);
-    } else setContacts([...contacts, newContact]);
-  };
+  //   if (checkArray.length > 0) {
+  //     alert(`Masz już kontakt o imieniu : ${newContact.name}`);
+  //   } else setContacts([...contacts, newContact]);
+  // };
   return (
     <>
       <Contacts/>
